69146078ba9db24837627b4ddd520fce27c9da8c
klava <klimasaus1@gmail.com>
2024-12-05 10:38:42 +0300

    fix context root in docker-compose

--- project/app/views.py
+++ project/app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import Person
 
 # Create your views here.
 def index(request: HttpRequest):
     context = request.GET
+    context.update({
+        'people': Person.objects.all()
+    })  
     return render(request, 'index.html', context)
